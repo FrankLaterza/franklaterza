48116ab7ffd64766272e74dfb950636f47386b94
FrankLaterza <franklaterza@gmail.com>
2022-07-15 16:57:06 -0400

    meta tags

---
+++ components/meta/metaTag.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+function MetaTags (){
+    return(
+    <Head>
+
+        <meta
+        title="Frank Laterza's Portfolio!"
+        name="Frank's website"
+        />
+        <meta name="theme-color" content='rgb(24, 24, 27)'/>
+    </Head>
+    )
+
+}
+
+export {MetaTags};
