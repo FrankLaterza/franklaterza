293ff081dc062b549f1e680affd7cd02375bc30c
FrankLaterza <franklaterza@gmail.com>
2022-07-21 16:39:10 -0400

    updated bolt

--- components/misc/bolts.tsx
+++ components/misc/bolts.tsx
@@ -1,13 +1,13 @@
-import { TbCircleHalfVertical } from "react-icons/Tb"
+import { AiOutlineMinusCircle } from "react-icons/Ai"
 
 // make sure parent has position: relative;
 function Bolts (){
   return (
     <div>
-    <TbCircleHalfVertical style={{ top: '0.5rem', left: '0.5rem', transform: 'rotate(-45deg)', color: 'black', position: 'absolute', width: "1rem", height: "1rem" }}/>
-    <TbCircleHalfVertical style={{ top: '0.5rem', right : '0.5rem', transform: 'rotate(45deg)', color: 'black', position: 'absolute', width: "1rem", height: "1rem" }}/>
-    <TbCircleHalfVertical style={{ bottom: '0.5rem', left: '0.5rem', transform: 'rotate(45deg)', color: 'black', position: 'absolute', width: "1rem", height: "1rem" }}/>
-    <TbCircleHalfVertical style={{ bottom : '0.5rem', right: '0.5rem', transform: 'rotate(-45deg)', color: 'black', position: 'absolute', width: "1rem", height: "1rem" }}/>
+    <AiOutlineMinusCircle style={{ top: '0.5rem', left: '0.5rem', transform: 'rotate(-45deg)', color: 'black', position: 'absolute', width: "1rem", height: "1rem" }}/>
+    <AiOutlineMinusCircle style={{ top: '0.5rem', right : '0.5rem', transform: 'rotate(45deg)', color: 'black', position: 'absolute', width: "1rem", height: "1rem" }}/>
+    <AiOutlineMinusCircle style={{ bottom: '0.5rem', left: '0.5rem', transform: 'rotate(45deg)', color: 'black', position: 'absolute', width: "1rem", height: "1rem" }}/>
+    <AiOutlineMinusCircle style={{ bottom : '0.5rem', right: '0.5rem', transform: 'rotate(-45deg)', color: 'black', position: 'absolute', width: "1rem", height: "1rem" }}/>
   </div>
   )
 }
