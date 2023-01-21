50bf6dcb9f48f9feab8a36705931e85a32f7bab1
frank <franklaterza@gmail.com>
2023-01-21 03:16:25 -0500

    new favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { NavBar } from '../components/navbar';  
+import "../styles/globals.css";
+import type {AppProps} from "next/app";
+import {NavBar} from "../components/navbar";
 import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
   return (
     <div>
       <Head>
-        <meta
-        title="Frank Laterza's Portfolio!"
-        name="Frank's website"/>
-        <meta 
-        name="theme-color"
-        content="#18181"/>
+        <meta title="Frank Laterza's Portfolio!" name="Frank's website" />
+        <meta name="theme-color" content="#18181" />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
       {/* navigatoin bar */}
-      <NavBar/>
+      <NavBar />
       <Component {...pageProps} />
     </div>
-    
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
